54d8e106288e69877456d53801d314bfc0751b21
David Nguyen <51886638+daviddhnguyen@users.noreply.github.com>
2024-01-16 21:07:02 -0800

    Create game round to compare player to computer

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-let computerSelection
-
 function getComputerChoice() {
     computerSelection = Math.floor(Math.random()*3 + 1);
   /* Assigns random int number between 1 - 3 as the computer choice. */
@@ -18,9 +16,6 @@ function getComputerChoice() {
   }
 }
 
-getComputerChoice()
-console.log(computerSelection);
-
 function playRound(playerSelection, computerSelection) {
     /*change playerSelection to lower case*/
     let playerSelectionLower = playerSelection.toLowerCase();
@@ -33,4 +28,41 @@ function playRound(playerSelection, computerSelection) {
 
     console.log(playerSelectionFormatted)
 
+    /*check to make sure player put rock paper or scissors */
+    if (playerSelectionFormatted == computerSelection) {
+        return "You tied!- Play again!";
+    } else if (playerSelectionFormatted == "Rock") {
+        switch (computerSelection) {
+            case "Paper":
+                return "Computer won!";
+                break;
+            case "Scissors":
+                return "You won!";
+                break;
+        }
+    } else if (playerSelectionFormatted == "Paper") {
+        switch (computerSelection) {
+            case "Scissors":
+                return "Computer won!";
+                break;
+            case "Rock":
+                return "You won!";
+                break;
+        }  
+    } else if (playerSelectionFormatted == "Scissors") {
+        switch (computerSelection) {
+            case "Rock":
+                return "Computer won!";
+                break;
+            case "Paper":
+                return "You won!";
+                break;
+        }  
+    } else {
+        return "Improper input, please enter Rock, Paper or Scissors."
     }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
