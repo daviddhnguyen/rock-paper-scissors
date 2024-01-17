ecdf80202392fd9090204b93643af8e4f204d4be
David Nguyen <51886638+daviddhnguyen@users.noreply.github.com>
2024-01-16 21:38:20 -0800

    Creates best-of-five game and allows player input

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function getComputerChoice() {
             computerSelection = 'Scissors';
     	    break;
     } 
-    console.log(computerSelection);
+    console.log("Computer: " + computerSelection);
     return computerSelection;
 }
 
@@ -26,11 +26,12 @@ function playRound(playerSelection, computerSelection) {
     let remainingLetters = playerSelectionLower.slice(1);
     let playerSelectionFormatted = firstLetter.concat(remainingLetters);
 
-    console.log(playerSelectionFormatted)
+    console.log("Player: " + playerSelectionFormatted)
 
-    /*check to make sure player put rock paper or scissors */
+    /*compares player vs computer
+    if player did not put rock paper or scissor give error to try again*/
     if (playerSelectionFormatted == computerSelection) {
-        return "You tied!- Play again!";
+        return "tied";
     } else if (playerSelectionFormatted == "Rock") {
         switch (computerSelection) {
             case "Paper":
@@ -59,10 +60,55 @@ function playRound(playerSelection, computerSelection) {
                 break;
         }  
     } else {
-        return "Improper input, please enter Rock, Paper or Scissors.";
+        return "Input error";
     }
 }
 
-const playerSelection = "rock";
+/* test code
+const playerSelection = "rOCk";
 const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
+*/
+
+function game() {
+    /*create best-of-five game and keep score. 
+    Report winner or loser once player or computer reaches 5*/
+    let pointsToWin = 5;
+    let playerScore = 0;
+    let computerScore = 0;
+
+
+
+    while (playerScore < pointsToWin && computerScore < pointsToWin) {
+
+        console.log(`Player Score: ${playerScore} \nComputer Score: ${computerScore}`)
+
+        let playerSelection = prompt("Enter Rock, Paper, or Scissors to play!")
+        switch (playRound(playerSelection,getComputerChoice())) {
+            case "tied":
+                console.log("You tied!- Play again!");
+                break;
+            case "Computer won!":
+                console.log("Computer won this round!");
+                computerScore += 1;
+                if (computerScore == pointsToWin) {
+                    console.log("Computer wins the game!");
+                    return
+                }
+                break;
+            case "You won!":
+                console.log("You won this round!");
+                playerScore += 1;
+                if (playerScore == pointsToWin) {
+                    console.log("You win the game!");
+                    return
+                }
+                break;
+            case "Input error":
+                console.log("Improper input, please enter Rock, Paper or Scissors.");
+                break;
+        }
+    }
+}
+
+game()
