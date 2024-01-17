65dada8e2b9bf34b9f1ee684e1c2d9ad13193fa7
David Nguyen <51886638+daviddhnguyen@users.noreply.github.com>
2024-01-16 21:02:07 -0800

    Format player selection to be case-insensitive

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
+let computerSelection
+
 function getComputerChoice() {
-	let computerSelection = Math.floor(Math.random()*3 + 1);
+    computerSelection = Math.floor(Math.random()*3 + 1);
   /* Assigns random int number between 1 - 3 as the computer choice. */
-  console.log(computerSelection);
-  switch (computerSelection) {
-    case 1:
-    	computerSelection = 'Rock';
-    	console.log(computerSelection);
-    	break;
-    case 2:
-      computerSelection = 'Paper';
-    	console.log(computerSelection);
-    	break;
-    case 3:
-    	computerSelection = 'Scissors';
-    	console.log(computerSelection);
-    	break;
+    console.log(computerSelection);
+    switch (computerSelection) {
+        case 1:
+    	    computerSelection = 'Rock';
+    	    break;
+        case 2:
+            computerSelection = 'Paper';
+            break;
+        case 3:
+            computerSelection = 'Scissors';
+    	    break;
+    return computerSelection
   }
 }
 
 getComputerChoice()
+console.log(computerSelection);
+
+function playRound(playerSelection, computerSelection) {
+    /*change playerSelection to lower case*/
+    let playerSelectionLower = playerSelection.toLowerCase();
+    /*Seperate the first letter and capitalize*/
+    let firstLetter = playerSelectionLower.charat(0);
+    firstLetter = firstLetter.toUpperCase();
+    /*slice remaining letters after first letter and concat with Uppercase first letter*/
+    let remainingLetters = playerSelectionLower.slice(1);
+    let playerSelectionFormatted = concat(firstLetter,remainingLetters);
+
+    console.log(playerSelectionFormatted)
+
+    }
